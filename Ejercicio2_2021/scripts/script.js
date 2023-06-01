b85cc013d38d59a503b2499037eb79e61557effb
Darío J. Ramírez <dariojesusramirez@gmail.com>
2023-06-01 21:30:37 +0200

    Faltan las funcionalidades del botón 3 y 4

--- Ejercicio2_2021/scripts/script.js
+++ Ejercicio2_2021/scripts/script.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
     $('#boton1').on('click', function() {
-        $('#salida_jquery').append('Origen de la imagen: ' + $('#imagen_persona').src().val());
+        $('#salida_jquery').empty();
+        $('#salida_jquery').append('<p>Origen de la imagen: ' + $('#imagen_persona').attr("src") + '</p>');
+        $('#salida_jquery').append('<p>Alto de la imagen: ' + $('#imagen_persona').height() + '</p>');
+        $('#salida_jquery').append('<p>Ancho de la imagen: ' + $('#imagen_persona').width() + '</p>');
+    });
+
+    $('#boton2').on('click', function() {
+        $('#sede').css('color', 'red');
+    });
+
+    $('#boton4').on('click', function() {
+        $('#imagen_resultado').css('border: 10px solid red; border-radius: 10px');
     });
 
 });
