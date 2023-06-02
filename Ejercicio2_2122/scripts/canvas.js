fb13c54f8e6b9469e4011afd39407457356e00b8
Darío J. Ramírez <dariojesusramirez@gmail.com>
2023-06-02 22:03:46 +0200

    Ejercicio2_2122 antes de la animación

---
+++ Ejercicio2_2122/scripts/canvas.js
@@ -0,0 +1,43 @@
+// este fichero tendrás que incorporarlo al desarrollo.
+// en el tenemos una zona de código que tendrás que completar para que se muestre lo que se pide en el ejercicio.
+
+
+// función para inicializar el contexto.
+function iniciaCanvas(idCanvas) {
+    var elemento = document.getElementById(idCanvas);
+    if (elemento && elemento.getContext) {
+        var contexto = elemento.getContext('2d');
+        if (contexto) {
+            return contexto;
+        }
+    }
+    return false;
+}
+
+// Este código hace referencia a un objeto canvas con identficador "canvas_inicio"
+window.onload = function() {
+    ctx_iniciar = iniciaCanvas("canvas_inicio");
+
+    //comprobamos el contexto
+    if (ctx_iniciar) {
+
+        ctx_iniciar.beginPath();
+        ctx_iniciar.lineWidth = 5;
+        ctx_iniciar.moveTo(5,5);
+        ctx_iniciar.lineTo(45,25);
+        ctx_iniciar.lineTo(5,45);
+        ctx_iniciar.closePath();
+
+        ctx_iniciar.fillStyle = "green";
+        ctx_iniciar.fill();
+
+        ctx_iniciar.strokeStyle = "red";
+        ctx_iniciar.stroke();
+
+        ctx_iniciar.canvas.addEventListener("click", function () {
+            document.getElementById("video").play();
+        });
+    }
+
+}
+
